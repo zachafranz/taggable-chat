04eaeb24b2e51285e15a306b40bbe6ef3dcb8b7e
Alejandro R <Alexrom789@Gmail.com>
2018-06-01 19:50:18 -0400

    set up post request and console logs

--- client/client.js
+++ client/client.js
@@ -3,21 +3,63 @@
 
 $(document).ready(function(){
 
-    $('#txtMessage').keypress(function (e) {
-        if (e.which == 13) {
-            //console.log('I was pressed')
-            var chatMessage = $("#txtMessage").val()
-            var username = $("#txtName").val()
-            $( "#chatlog" ).append(username + ": " + chatMessage + "<br>");
-          return false;  
-        }
-      });
+    ////////////////
 
-    $("#send").click(function() {
-        //console.log('I was clicked')
-        var chatMessage = $("#txtMessage").val()
-        var username = $("#txtName").val()
-        $( "#chatlog" ).append(username + ": " + chatMessage + "<br>");
-    })
+    function postChat(message){
+        console.log('ENTERING POSTCHAT! / SENDING CHATMESSAGE TO SERVER');
+        $.post("http://localhost:3000/message", message)
+        console.log('Message: ', message);
+    }
+
+         $("#send").click(() => {
+            console.log('ENTERING CLICK! / CREATING CHAT MESSAGE VAR');
+            var chatMessage = {
+                name: $("#txtName").val(), message: $("#txtMessage").val()
+            }
+            postChat(chatMessage)
+            console.log('ChatMessage', chatMessage);
+        })
+
+        $('#txtMessage').keypress(function (e) {
+            if (e.which == 13) {
+                //console.log('I was pressed')
+                var chatMessage = {
+                    name: $("#txtName").val(), message: $("#txtMessage").val()
+                }
+                postChat(chatMessage)
+                console.log('ChatMessage', chatMessage);
+              return false;  
+            }
+        });
+
+        // $.when( $("#send").click(() => {
+        //     console.log('in click');
+        //     var chatMessage = {
+        //         name: $("#txtName").val(), message: $("#txtMessage").val()
+        //     }
+        //     //postChat(message)
+        // })).then(postChat(chatMessage))
+    
+    
+    ////////////
+    
+    // $('#txtMessage').keypress(function (e) {
+    //     if (e.which == 13) {
+    //         //console.log('I was pressed')
+    //         var chatMessage = $("#txtMessage").val()
+    //         var username = $("#txtName").val()
+    //         $( "#chatlog" ).append(username + ": " + chatMessage + "<br>");
+    //       return false;  
+    //     }
+    //   });
+
+    // $("#send").click(function() {
+    //     //console.log('I was clicked')
+    //     var chatMessage = $("#txtMessage").val()
+    //     var username = $("#txtName").val()
+    //     $( "#chatlog" ).append(username + ": " + chatMessage + "<br>");
+    // })
+
+    
 })
 
