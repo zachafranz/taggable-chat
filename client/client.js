eaf4da5b188f761f5a19fffac5c75a3e4d60e75f
Alejandro R <Alexrom789@Gmail.com>
2018-06-01 16:18:30 -0400

    completed basic front end design

---
+++ client/client.js
@@ -0,0 +1,23 @@
+//console.log('in here')
+//username = "User"
+
+$(document).ready(function(){
+
+    $('#txtMessage').keypress(function (e) {
+        if (e.which == 13) {
+            //console.log('I was pressed')
+            var chatMessage = $("#txtMessage").val()
+            var username = $("#txtName").val()
+            $( "#chatlog" ).append(username + ": " + chatMessage + "<br>");
+          return false;  
+        }
+      });
+
+    $("#send").click(function() {
+        //console.log('I was clicked')
+        var chatMessage = $("#txtMessage").val()
+        var username = $("#txtName").val()
+        $( "#messages" ).append(username + ": " + chatMessage + "<br>");
+    })
+})
+
