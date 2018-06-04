61009829aaba1f357c717d121cb69512ecc91489
root <root@DESKTOP-5P1J73P.localdomain>
2018-06-04 19:19:08 -0400

    added last 10 messages feature

--- client/client.js
+++ client/client.js
@@ -1,38 +1,63 @@
 //console.log('in here')
 //username = "User"
 
-$(document).ready(function(){
-    // grab created_by and message values  
-    $("#send").click(() => {
-      var chatMessage = { name: $("#txtName").val(), message: $("#txtMessage").val() };
-      $.post("http://localhost:3000/message", chatMessage);
-    });
+const createMessageHtml = (messageObj) => {
+  // Takes in obj with expected fields: _id, created_by, message
 
-    $('#txtMessage').keypress(function (e) {
+  console.log(messageObj);
+  let divElem = $("<div></div>")
+
+  let tagElem = $("<p></p>").addClass('tagsClass hiddenTags');
+  messageObj.tag.forEach((elem) => {
+    tagElem.append($("<span></span>").text(elem).addClass('tagSpanClass'));
+  });
+
+  let messageElem = $("<p></p>").text(messageObj.created_by + ': ' + messageObj.message);
+  messageElem.addClass('messageClass notTagable').attr('id', messageObj._id);
+  messageElem.click((elem) => {
+    if (taggingAction) {
+      console.log('Message was clicked', elem.target.id);
+      let $elem = $('#' + elem.target.id);
+      $elem.hasClass('tagged') ? $elem.removeClass('tagged') : $elem.addClass('tagged')
+    }
+  })
+  divElem.append(messageElem, tagElem);
+  return divElem;
+}
+
+$(document).ready(function () {
+  // grab created_by and message values  
+  $("#send").click(() => {
+    var chatMessage = { name: $("#txtName").val(), message: $("#txtMessage").val() };
+    $.post("http://localhost:3000/message", chatMessage);
+  });
+
+  $('#txtMessage').keypress(function (e) {
     if (e.which == 13) {
       //console.log('I was pressed')
       var chatMessage = { name: $("#txtName").val(), message: $("#txtMessage").val() };
       $.post("/message", chatMessage);
       chatMessage.message = '';
-      return false;  
+      return false;
     }
-    });
+  });
 
-    var length = 0;
-    setInterval(() => {
-      var response = '';
-      $.ajax({ 
-        type: "GET",   
-        url: "/message",   
-        async: false,
-        success : function(text) {
-          response = text;
-          if(response.length > length) {
-            $( "#chatlog" ).append(`${response[response.length - 1].created_by}: ${response[response.length - 1].message}<br>`);   
-            length = response.length;
+  let numOfMessages = -10;
+  let displayedMessages = [];
+  setInterval(() => {
+    $.ajax({
+      type: "GET",
+      url: "/message",
+      async: false,
+      success: function (response) {
+        response.slice(numOfMessages).forEach((message) => {
+          if (!displayedMessages.includes(message._id)) {
+            displayedMessages.push(message._id)
+            $("#chatlog").append(createMessageHtml(message));
           }
-        }
-      });
-  }, 200);
+        });
+      }
+    });
+  }, 3000);
 })
 
