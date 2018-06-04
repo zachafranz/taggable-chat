f42c68a7bfbd0c181cf14033c1f697646b8022d5
Zach Franz <zachfranz@Zachs-MacBook-Pro.local>
2018-06-04 19:19:29 -0400

    Adding Tag Feature

--- client/client.js
+++ client/client.js
@@ -1,38 +1,122 @@
-//console.log('in here')
-//username = "User"
+/* eslint-disable */
+
+const createMessageHtml = (messageObj) => {
+  // Takes in obj with expected fields: _id, created_by, message
+
+  console.log(messageObj);
+  let divElem = $("<div></div>")
+
+  let tagElem = $("<p></p>").addClass('tagsClass hiddenTags');
+  messageObj.tag.forEach((elem) => {
+    tagElem.append($("<span></span>").text(elem).addClass('tagSpanClass').data('messId', messageObj._id));
+    tagElem.click((event) => {
+      if (deletingTags) {
+        console.log('Remove this from tag array', $(event.target).data('messId'), event.target.innerText);
+        $.post(
+          "/deleteTag", 
+          { messageId: $(event.target).data('messId'), tagText: event.target.innerText },
+          (data) => {
+            console.log('Done with tag delete', data);
+          });
+      }
+    })
+  });
+
+  let messageElem = $("<p></p>").text(messageObj.created_by + ': ' + messageObj.message);
+  messageElem.addClass('messageClass notTagable').attr('id', messageObj._id);
+  messageElem.click((event)=> {
+    if (taggingAction) {
+      console.log('Message was clicked', event.target.id);
+      let $elem = $('#' + event.target.id);
+      $elem.hasClass('tagged') ? $elem.removeClass('tagged') : $elem.addClass('tagged')
+    }
+  })
+  divElem.append(messageElem, tagElem);
+  return divElem;
+}
+
+let taggingAction = false;
+let deletingTags = false;
 
 $(document).ready(function(){
-    // grab created_by and message values  
-    $("#send").click(() => {
-      var chatMessage = { name: $("#txtName").val(), message: $("#txtMessage").val() };
-      $.post("http://localhost:3000/message", chatMessage);
-    });
 
-    $('#txtMessage').keypress(function (e) {
+  $("#send").click(() => {
+    var chatMessage = { name: $("#txtName").val(), message: $("#txtMessage").val() };
+    $.post("http://localhost:3000/message", chatMessage);
+  });
+
+  $('#deleteTagsBtn').click(() => {
+    console.log($(".tagSpanClass").data('messId'));
+    console.log('Clicked delete tags'); // deletableTagClass
+    if (deletingTags) {
+      $(".tagSpanClass").removeClass("deletableTagClass");
+      deletingTags = false;
+    } else {
+      $(".tagSpanClass").addClass("deletableTagClass");
+      deletingTags = true;
+    }
+  });
+
+  $("#hideOrShowTagsBtn").click(() => {
+    console.log('Clicked hide/show tags');
+    if ($('#hideOrShowTagsBtn').text() === 'Show Tags') {
+      $("#hideOrShowTagsBtn").text('Hide Tags');
+      $(".tagsClass").removeClass("hiddenTags");
+    } else {
+      $("#hideOrShowTagsBtn").text('Show Tags');
+      $(".tagsClass").addClass("hiddenTags");
+    }
+  });
+
+  $("#selectMessagesBtn").click(() => {
+    console.log('Clicked select messages');
+    if (!taggingAction) {
+      $("#selectMessagesBtn").text('Nevermind');
+      $(".messageClass").addClass("tagable");
+      taggingAction = true;
+    } else {
+      $("#selectMessagesBtn").text('Select Messages to Tag');
+      $(".messageClass").removeClass("tagable tagged");
+      taggingAction = false;
+    }
+  });
+
+  $("#tagMessagesBtn").click(() => {
+    let messageIdArr = [];
+    $('.tagged').each((ind, elem) => {
+      messageIdArr.push(elem.id);
+      console.log(elem.id);
+    });
+    $.post("/tag", { tagIdArr: messageIdArr, tagText: $('#tageNameInput').val() });
+  });
+
+  $('#txtMessage').keypress(function (e) {
     if (e.which == 13) {
-      //console.log('I was pressed')
       var chatMessage = { name: $("#txtName").val(), message: $("#txtMessage").val() };
       $.post("/message", chatMessage);
-      chatMessage.message = '';
-      return false;  
     }
-    });
+  });
 
-    var length = 0;
-    setInterval(() => {
-      var response = '';
-      $.ajax({ 
-        type: "GET",   
-        url: "/message",   
-        async: false,
-        success : function(text) {
-          response = text;
-          if(response.length > length) {
-            $( "#chatlog" ).append(`${response[response.length - 1].created_by}: ${response[response.length - 1].message}<br>`);   
-            length = response.length;
-          }
+  // Add initial messages
+
+  var length = 0;
+  setInterval(() => {
+    var response = '';
+    $.ajax({ 
+      type: "GET",   
+      url: "/message",   
+      async: false,
+      success : function(text) {
+        console.log(text);
+        console.log(length);
+        response = text;
+        if(response.length > length) {
+          let messageToAppend = createMessageHtml(response[response.length - 1]);
+          $('#chatlog').append(messageToAppend);
+          length = response.length;
         }
-      });
-  }, 200);
+      }
+    });
+  }, 2000);
 })
 
--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ const app = express();
 
 //********  conneect to mongo database
 mongoose.connect('mongodb://ddeste01:Robert123!@ds029824.mlab.com:29824/chatapp');
-
+mongoose.connection.once('open', () => {
+  console.log('Connected to Database');
+});
+ 
 //********  express server routes
 app.use(express.static(path.join(__dirname, './../client')));
 // extended true === deep parsing
@@ -18,11 +21,14 @@ app.use(bodyParser.json());
 
 //******** routes
 app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.get('/message', messageController.getMessages);
 app.post('/message', messageController.postMessage);
+
+app.post('/tag', messageController.tagMessages);
+app.post('/deleteTag', messageController.deleteTag);
 
 app.listen(3000, () => {
   console.log('listening on Port 3000');
