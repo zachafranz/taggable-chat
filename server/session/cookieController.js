54194dd903b9ae74f348e90e757e64e2da41f5b4
Shafayat Alam <shafayatalam05@gmail.com>
2018-06-06 14:55:33 -0400

    Updated cookie verification to check if user exists in Database

--- server/session/cookieController.js
+++ server/session/cookieController.js
@@ -16,11 +16,21 @@ const cookieController = {
   checkCookie: (req, res, next) => {
     if (req.cookies.TagChatUserName) {
       console.log('Our cookie: ', req.cookies);
-      next();
+      User.findOne({userName: req.cookies.TagChatUserName}).then((foundUser) => {
+        if (foundUser) {
+          console.log('Valid cookie, username exists: ', foundUser)
+          next();
+        }
+        else res.redirect('/');
+      }).catch((err) => {
+        console.log('There was an error validating user. Please retry.')
+        res.status(400).send('There was an error validating user. Please retry.')
+      });   
     } else {
       res.redirect('/');
     }
   }
 }
+
 
 module.exports = cookieController;
