6edabf90f75a62471ac3610caabb7f1cf2a44d9d
root <root@DESKTOP-5P1J73P.localdomain>
2018-06-06 19:02:28 -0400

    Final Changes by Mahfuz

--- server/user/UserController.js
+++ server/user/UserController.js
@@ -1,18 +1,16 @@
 const User = require('./UserModel');
+const bcyrpt = require('bcrypt');
 
 const UserController = {
-  loginUser(req, res) {
-    console.log('User info sent', req.body);
-    User.findOne({
-      userName: req.body.userName,
-      // password: req.body.password
-    }).then((foundUser) => {
-      console.log('foundUser in DB', foundUser);
-      // if (!foundUser) res.send('The username and/or password combination does not exist. Please retry.');
-      User.authenticate(foundUser.userName, req.body.password, (err, user) => {
-        if (err) res.send(err);
-        res.send(user);
-      });
+  loginUser(req, res, next) {
+    User.findOne({ userName: req.body.userName }).then((foundUser) => {
+      if (!foundUser) res.send('The username and/or password combination does not exist. Please retry.');
+      else {
+        bcyrpt.compare(req.body.password, foundUser.password, (err, result) => {
+          if (err || result === false) res.send('The username and/or password combination does not exist. Please retry.');
+          res.send(foundUser.userName);
+        });
+      }
     }).catch((err) => {
       res.status(400).send('There was an error; please try logging in again.');
     });
--- server/user/UserModel.js
+++ server/user/UserModel.js
@@ -17,34 +17,10 @@ const userSchema = new Schema({
   }
 });
 
-//Authenticate input against database
-//statics is how you add methods to a model i.e. userSchema model
-userSchema.statics.authenticate = function (userName, password, next) {
-  console.log("we made it to authenticate");
-  //find the user from the database
-  User.findOne({ userName: userName }).exec((err, user) => {
-    //for all else - you have found a user in the db...now authenticate pw
-    //Use bcrypt to authenticate password by comparing
-    console.log("this is authenticate user: ", user)
-    bcyrpt.compare(password, user.password, (err, result) => {
-      console.log("this is bcrypt result: ", result);
-      console.log(password, user.password);
-      if (err) return err;
-      if (result === true) return next(null, user);
-      else return next();
-    });
-  });
-};
-//Hash password before saving it to the database
 userSchema.pre('save', function (next) {
-  //I want to apply this to each user
-  let user = this;
-  console.log("Inside of my Pre Hook: ", this);
-  //hash takes in - data, salt, callback
-  bcyrpt.hash(user.password, 10, (err, hash) => {
+  bcyrpt.hash(this.password, 10, (err, hash) => {
     if (err) return next(err);
-    //assign the password to the hash
-    user.password = hash;
+    this.password = hash;
     return next();
   });
 });
